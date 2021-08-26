9d582db48e3533d374d2334af4766b0bea91f308
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-26 08:33:20 -0700

    add drop and drag functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,50 @@ var loadTasks = function () {
   });
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  update: function (event) {
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+      });
+
+    // trim down list's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    saveTasks();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  },
+});
+
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
